23d68f66ed2ba7a36fd3b142a3155a0400eda4d7
Thom Smith <thom@thomthesmith.com>
2021-02-04 14:42:08 -0500

    Initial actual commit.

---
+++ ModEntry.cs
@@ -0,0 +1,59 @@
+﻿using System;
+// using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+using ContentPatcher;
+
+namespace SDVTest
+{
+    public class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.GameLoop.GameLaunched += this.OnGameLaunched;
+        }
+
+        private void OnGameLaunched(object sender, GameLaunchedEventArgs e)
+        {
+            var api = this.Helper.ModRegistry.GetApi<IContentPatcherAPI>("Pathoschild.ContentPatcher");
+
+            api.RegisterToken(this.ModManifest, "Luck", () => {
+                if (Context.IsWorldReady)
+                {
+                    return new[] { EvaluateLuck(Game1.player.DailyLuck) };
+                }
+                else
+                {
+                    return null;
+                }
+            });
+        }
+
+        private string EvaluateLuck(double luck)
+        {
+            if (luck > 0.07)
+            {
+                return "best";
+            }
+            else if (luck > 0.02)
+            {
+                return "good";
+            }
+            else if (luck >= -0.02)
+            {
+                return "neutral";
+            }
+            else if (luck >= -0.07)
+            {
+                return "bad";
+            }
+            else
+            {
+                return "worst";
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# SDV-CrystalBall
-Adds a crystal ball that changes color depending on your daily luck.
+# Crystal Ball
+
+Adds a crystal ball that changes color depending on your daily luck. You can carry it around or set it down on a table.
+
+![Crystal ball on table](./crystal-ball-on-table.png)
+
+To acquire the recipe, attain eight hearts with the Wizard. You will receive a letter the next day with the recipe attached.
+
+## Dependencies
+
+This mod requires the following other mods installed in order to run.
+
+- [SMAPI](https://smapi.io/).
+- [JSON Assets](https://www.nexusmods.com/stardewvalley/mods/1720).
+- [Content Patcher](https://www.nexusmods.com/stardewvalley/mods/1915).
+- [Mail Framework Mod](https://www.nexusmods.com/stardewvalley/mods/1536).
+
+## Installation
+
+Download the zipfile, unzip it, and place the Crystal Ball directory in your Mods directory.
+
+## About
+
+This mod is available under the terms of the MIT license.
+
+The crystal ball sprites were created by Mary Helen Perot.
